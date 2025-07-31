d23d0b1aacfaca010f3711168b35b9804f9dcf83
JaninaAlona <jelona@posteo.net>
2025-07-31 19:50:12 +0200

    reset.css added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Welcome from './components/Welcome.jsx'
 
 function App() {
-  const [message, setMessage] = useState('');  
-
     useEffect(() => {  
         fetch('/api/meetingvals')  
             .then((res) => console.log("Something coming")) 
@@ -13,7 +11,6 @@ function App() {
   return (
     <>
       <h1>Crabtalk</h1>
-      <div>{message}</div>
       <Welcome />
     </>
   )
