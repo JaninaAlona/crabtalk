2d494c470a3747411eb9a6304d752a234742fe5a
JaninaAlona <jelona@posteo.net>
2026-02-15 23:33:15 +0100

    uuid implemented

--- src/components/CreateMeeting.jsx
+++ src/components/CreateMeeting.jsx
@@ -1,9 +1,9 @@
-import { getSHA256Hash } from "boring-webcrypto-sha256";
+import {v4 as uuidv4} from 'uuid';
 import Password from './Password.jsx';
 import MeetingType from './MeetingType.jsx';
 import Participants from './Participants.jsx';
 
-function CreateMeeting({transferLink, submitTrigger}) {
+function CreateMeeting({transferLink}) {
 
     async function getSHA256Hash(input) {
         const textAsBuffer = new TextEncoder().encode(input);
@@ -14,48 +14,18 @@ function CreateMeeting({transferLink, submitTrigger}) {
             .join("");
         return hash;
     }
-    
-    function randomChar(length, chars) {
-        let result           = '';
-        const characters       = chars;
-        const charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
 
-    async function calculateMeetingLink() {
-        let meetingLink = '';
-        const performanceTime = performance.now()
-        const systemTime = new Date().getTime()
-
-        const smallestRand = Math.random()
-        const oneDigitRand = Math.random() * 10
-        const twoDigitRand = Math.random() * 100 
-        const threeDigitRand = Math.random() * 1000
-        const randomCharNoCaseDigitsA = randomChar(4, 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
-        const upperCaseA = randomChar(12, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        const lowerCaseA = randomChar(2, 'abcdefghijklmnopqrstuvwxyz')
-        const randomCharNoCaseDigitsB = randomChar(16, 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
-        const upperCaseB = randomChar(8, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        const lowerCaseB = randomChar(10, 'abcdefghijklmnopqrstuvwxyz')
-        const calcString = lowerCaseA + performanceTime + lowerCaseB + upperCaseA + systemTime + smallestRand + oneDigitRand + twoDigitRand + randomCharNoCaseDigitsB + upperCaseB + threeDigitRand + randomCharNoCaseDigitsA 
-
-        let subA = calcString.substring(0, 21)
-        let subB = calcString.substring(30, 44)
-        let subC = calcString.substring(50, 80)
-        let subD = calcString.substring(98, calcString.length)
-        const input = subA + subB + subC + subD
-        console.log(input)
-        meetingLink = await getSHA256Hash(input)
+    async function generateMeetingLink() {
+        const uuid = uuidv4();
+        console.log(`UUID: ${uuid}`)
+        const meetingLink = await getSHA256Hash(uuid)
         return meetingLink
     }   
 
     async function createSubmit(e) {
         e.preventDefault()
-        const meetingLink = await calculateMeetingLink()
-        console.log(meetingLink)
+        const meetingLink = await generateMeetingLink()
+        console.log(`Meeting Link: ${meetingLink}`)
         transferLink(meetingLink)
     }
 
--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useRef } from 'react';
 
 function Participants(props) {
