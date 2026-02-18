8ec6ab91aebb613871f2ec003eb20714b4c8c0a8
JaninaAlona <jelona@posteo.net>
2026-02-18 02:19:26 +0100

    fixed hide/display password

--- src/components/CustomCheckbox.jsx
+++ src/components/CustomCheckbox.jsx
@@ -2,7 +2,7 @@ function CustomCheckbox(props) {
     return (
         <>
             <label className="checkbox-label" htmlFor={props.htmlFor}>{props.label}
-                <input ref={props.ref} type="checkbox" id={props.id} name={props.name} value={props.value} checked={props.checked} onChange={props.onChange} />
+                <input type="checkbox" id={props.id} name={props.name} value={props.value} checked={props.checked} onChange={props.onChange} />
                 <span className="checkmark"></span>
             </label>
         </>
--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,23 +1,32 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import CustomCheckbox from './CustomCheckbox.jsx';
 
 function Password(props) {
-    let pwInput = useRef(null)
-    let showPWToggle = useRef(null)
+    const [pwInputType, setPwInputType] = useState('password');
+    const [pwValue, setPwValue] = useState('');
+    const [isChecked, setChecked] = useState(false);
     
     function onPWVisibilityChange() {
-        if (showPWToggle.current.checked === true) {
-            pwInput.current.type = "text"
+        if (isChecked === false) {
+            setPwInputType('text');
         } else {
-            pwInput.current.type = "password"
+            setPwInputType('password');
         }
     }
-    
+
+    function onPWInput() {
+        if (isChecked === true) {
+            setPwInputType('text');
+        } else {
+            setPwInputType('password');
+        }
+    }
+        
     return (
         <div className="welcome-gui-con">
             <label htmlFor={props.pwID}>{props.pwLabel}</label>
-            <input ref={pwInput} className="welcome-input" type="password" id={props.pwID} name={props.pwName} placeholder="none" />
-            <CustomCheckbox ref={showPWToggle} id={props.id} name={props.name} htmlFor={props.htmlFor} label={props.label} value={props.value} checked={props.checked}onChange={onPWVisibilityChange} />
+            <input className="welcome-input" type={pwInputType} id={props.pwID} name={props.pwName} value={pwValue} onChange={e => { setPwValue(e.target.value); onPWInput() }} />
+            <CustomCheckbox id={props.id} name={props.name} htmlFor={props.htmlFor} label={props.label} value={props.value} checked={isChecked} onChange={e => { setChecked(e.target.checked); onPWVisibilityChange() }} />
         </div>
     )
 }
