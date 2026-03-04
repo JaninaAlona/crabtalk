81b7bc0c3c479db75eda4ccf128026d126c7e06e
JaninaAlona <jelona@posteo.net>
2026-03-04 10:20:53 +0100

    Reconstructing Backend

--- src/crabtalk-backend/api/meetingvals.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import MeetingValsCtrl from "./meetingvals.controller.js"
-
-const route = express.Router()
-
-route.route("/api/meetingvals").post(MeetingValsCtrl.apiPostMeetingvals)
-
-export default route
